04521da6322bffa86df5600d735900885b5e4057
Peter Meehan <meehanp2@tcd.ie>
2017-03-10 01:32:27 +0000

    made test payloads multiline, posts to ec2 :seedling:

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 var request = require("request");
 
 var options = { method: 'POST',
-  url: 'http://localhost:4001/execute',
+  url: `http://${process.env.CAISSON_EC2_IP}:4001/execute`,
   headers:
   { 'cache-control': 'no-cache',
     'content-type': 'application/x-www-form-urlencoded' },
   form:
   { timelimit: '3',
     memlimit: '1000',
-    payload: 'id -u -n; id -u; ping 8.8.8.8; pwd; grep Cap /proc/self/status; dd if=/dev/urandom bs=3 count=1',
-    lang: 'bash' }
-    // payload: 'print(410 + 10)',
-    // lang: 'python' }
-    // payload: '#include<stdio.h>\nint main() { printf("Hello World\\n");  }',
-    // lang: 'c' }
+
+    // lang: 'bash',
+    // payload: `
+    //   echo hi;
+    //   rm -rf /; # this is safe now ¯\_(ツ)_/¯
+    //   sleep 60; # this is prevented now
+    // `}
+
+    lang: 'bash',
+    payload: `
+      pwd;
+      id -u -n;
+      id -u;
+      ping 8.8.8.8;
+      grep Cap /proc/self/status;
+      dd if=/dev/urandom bs=3 count=1;
+    `}
+
+    // lang: 'python',
+    // payload: `
+// print(410 + 10) # silly whitespace sensitve lanugages
+    // `}
+
+    // lang: 'c',
+    // payload: `
+    //   #include<stdio.h>
+    //   int main() {
+    //     printf("Hello World\\n");
+    //   }
+    // `}
 };
 
 request(options, function (error, response, body) {
